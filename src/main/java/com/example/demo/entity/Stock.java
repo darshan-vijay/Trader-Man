b90d4fe88f23b1ac52d6ccb78cf77277c6f565ff
Venkat <email@email.com>
2021-08-13 16:18:29 +0530

    JPA and Swagger initial config

---
+++ src/main/java/com/example/demo/entity/Stock.java
@@ -0,0 +1,69 @@
+package com.example.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Stock {
+	@Id
+	private int id;
+	private String stockTicker;
+	private double price;
+	private int volume;
+	private String buyOrSell;
+	private int statusCode;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getStockTicker() {
+		return stockTicker;
+	}
+
+	public void setStockTicker(String stockTicker) {
+		this.stockTicker = stockTicker;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getVolume() {
+		return volume;
+	}
+
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+
+	public String getBuyOrSell() {
+		return buyOrSell;
+	}
+
+	public void setBuyOrSell(String buyOrSell) {
+		this.buyOrSell = buyOrSell;
+	}
+
+	public int getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(int statusCode) {
+		this.statusCode = statusCode;
+	}
+
+}
